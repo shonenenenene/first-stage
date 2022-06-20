801bdad69334a892eae2164b12d10d6d7b5a4587
shonenenenene <danya.sytaev@gmail.com>
2022-06-20 13:48:44 +0300

    add some 2

---
+++ part1/ch2/15task.js
@@ -0,0 +1,23 @@
+// 2
+function checkAge(age) {
+   age < 18 ? true : confirm('Родители разрешили?')
+}
+  
+// function checkAge(age) {
+//     return (age > 18) || confirm('Родители разрешили?');
+//   }
+
+// 3
+
+function min( a, b ) {
+    return Math.min(a, b)
+}
+
+console.log( min( 76, -100 ) )
+
+// 4
+
+function pow( x, n ) {
+    return Math.pow(x, n)
+}
+

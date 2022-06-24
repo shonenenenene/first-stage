70f97d88019061591033f794acf394b7037d239c
shonenenenene <danya.sytaev@gmail.com>
2022-06-24 12:41:13 +0300

    stav` class!

---
+++ part1/ch9/03task.js
@@ -0,0 +1,12 @@
+// 1
+
+class Rabbit extends Object {
+    constructor( name ) {
+      super(name)  
+      this.name = name
+    }
+  }
+  
+  let rabbit = new Rabbit("Кроль")
+  
+  alert( rabbit.hasOwnProperty('name') )
